033e722472993512be768321d661b2a44ab4c405
Billy White <billywhite4@gmail.com>
2013-01-30 15:48:41 -0600

    additional exceptions for various total page scenarios

--- better-pagination.php
+++ better-pagination.php
@@ -26,7 +26,8 @@
 <?php
 	# some application variables
 	$element = $content_xml->content->element; # set path relative to Content XML
-	$query_string = preg_replace('/"/', '%22', $element->query);
+	$query_string = $element->query;
+	$query_string = preg_replace('/"/', '%22', $query_string);
 
 	# primary function to print individual page links
 	function printItems($element, $i) {
@@ -67,17 +68,17 @@
 	$set_page = $content_xml->content->element->links->list->set_page; # the current page number
 
 	# loops through total number of pages and handles use cases
-	# displays first three and last two page links, with the middle abbreviated with unavailable dots by default
 	for ($i = 1; $i <= $total_pages; $i++) {
+		# displays first three and last two page links, with the middle abbreviated with unavailable dots by default
 		if ($i<=3) {
 			printItems($element, $i);
-			if ($i==3 && $set_page==3) {
+			if ($i==3 && $set_page==3 && $total_pages!=3 && $total_pages!=4) {
 				printItems($element, 4);
 			}
 		}
 		
 		# print unavailable dots after 3rd page link except when on 4th or 5th
-		if ($i==3 && $set_page!=4 && $set_page!=5) {
+		if ($i==3 && $set_page!=4 && $set_page!=5 && $total_pages>5 && ($set_page!=3 && $i!=4)) {
 			unavailable();
 		}
 		
@@ -85,18 +86,45 @@
 		if ($i<6 && $i==$set_page) { #special consideration for 4th and 5th page links
 			if ($i==4) {
 				printItems($element, 4);
-				printItems($element, 5);
+				if ($total_pages!=4 && $total_pages !=5){
+					printItems($element, 5);
+				}
 			} elseif ($i==5) {
 				printItems($element, 4);
 				printItems($element, 5);
 				printItems($element, 6);
 			} 
-		} elseif($i == $next_to_last_page || $i == $total_pages) { # special considerations for last and next-to-last page links
+		} elseif(($i == $next_to_last_page || $i == $total_pages) && $total_pages>3 && ($set_page!=5 || $total_pages!=6)) { # special considerations for last and next-to-last page links
 			if ($set_page==$next_to_last_page && $i==$next_to_last_page) {
 				printItems($element, $set_page-1);
 			}
-			printItems($element, $i);
-		} elseif ($set_page>5 && $i>5 && $set_page<($total_pages-2) && $i<($total_pages-2) && $i==$set_page) { # handles all page links above 5 and less than second-from-last
+			if ($total_pages==4) { # special considerations for total pages between 4-7
+				printItems($element, 4);
+				break;
+			} elseif ($total_pages==5) {
+				if ($set_page == 4) {
+					
+				} elseif ($set_page!=3) {
+					printItems($element, 4);
+				}
+				printItems($element, 5);
+				break;
+			} elseif ($total_pages==6) {
+				if ($set_page!=4) {
+					printItems($element, 5);
+				}
+				printItems($element, 6);
+				break;
+			} elseif ($total_pages==7) {
+				if ($set_page!=5) {
+					printItems($element, 6);
+				}
+				printItems($element, 7);
+				break;
+			} else {
+				printItems($element, $i);
+			}
+		} elseif ($set_page>5 && $i>5 && $set_page<($total_pages-2) && $i<($total_pages-2) && $i==$set_page) { # handles all page links above five and less than second-from-last
 			printItems($element, ($set_page-1));
 			printItems($element, $set_page);
 			printItems($element, ($set_page+1));
@@ -106,7 +134,7 @@
 		}
 		
 		# preceding unavailable dots before last two page links when page is not directly in series with either first or last page links
-		if ($set_page>3 && $i==($total_pages-2) && $set_page!=$total_pages-3 && $set_page!=$total_pages-2 && $set_page!=$next_to_last_page && $set_page!=$total_pages) {
+		if ($set_page>2 && $i==($total_pages-2) && $set_page!=$total_pages-3 && $set_page!=$total_pages-2 && $set_page!=$next_to_last_page && $set_page!=$total_pages) {
 			unavailable();
 		}
 		
